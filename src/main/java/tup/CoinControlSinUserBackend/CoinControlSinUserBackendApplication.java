de52b61b8042d8fa24eeb28d899e3f0251c4e0d0
Fiama Arrúa <fiamaarrua5@gmail.com>
2023-09-08 14:34:38 -0300

    Guardar id de usuario logueado

--- src/main/java/tup/CoinControlSinUserBackend/CoinControlSinUserBackendApplication.java
+++ src/main/java/tup/CoinControlSinUserBackend/CoinControlSinUserBackendApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CoinControlSinUserBackendApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(CoinControlSinUserBackendApplication.class, args);
 	}
