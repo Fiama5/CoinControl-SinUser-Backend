7909d5fa7afbd09fe0cbdfcf01963bdc84245cba
Fiama Arrúa <fiamaarrua5@gmail.com>
2023-09-30 19:11:37 -0300

    Modificacion en el metodo add funds

--- src/main/java/tup/CoinControlSinUserBackend/controller/FundsController.java
+++ src/main/java/tup/CoinControlSinUserBackend/controller/FundsController.java
@@ -32,9 +32,28 @@ public class FundsController {
     }
 
     @PostMapping("/add")
-    public ResponseEntity<Funds> createFunds(@RequestBody Funds funds) {
-        Funds savedFunds = fundsRepository.save(funds);
-        return new ResponseEntity<Funds>(savedFunds, HttpStatus.CREATED);
+    public ResponseEntity<Funds> createOrUpdateFunds(@RequestBody Funds funds) {
+        // Buscar el fondo existente para el usuario y el predefinedFund
+        // Se utiliza el objeto fundsRepository para buscar en la base de datos si ya
+        // existe un registro de fundsUser y el PredefinedFund
+        // especificados en el objeto funds recibido en la solicitud.
+        Funds existingFunds = fundsRepository.findByUserAndPredefinedFund(funds.getUser(), funds.getPredefinedFund());
+
+        if (existingFunds != null) {
+            // Si existingFunds no es nulo, siginifica que se encontro un registro en
+            // fundsRepository
+            // Si existe, actualizo el monto
+            existingFunds.setAmount(existingFunds.getAmount() + funds.getAmount());
+            // Se actualiza el monto existente sumándole el monto del objeto funds recibido
+            // en la solicitud.
+            Funds updatedFunds = fundsRepository.save(existingFunds);
+            // Se guarda el monto en los fondos existentes
+            return new ResponseEntity<>(updatedFunds, HttpStatus.OK);
+        } else {
+            // Si no existe, se crea un nuevo registo
+            Funds savedFunds = fundsRepository.save(funds);
+            return new ResponseEntity<>(savedFunds, HttpStatus.CREATED);
+        }
     }
 
     // Traer todos los fondos
--- src/main/java/tup/CoinControlSinUserBackend/repository/FundsRepository.java
+++ src/main/java/tup/CoinControlSinUserBackend/repository/FundsRepository.java
@@ -6,7 +6,10 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import tup.CoinControlSinUserBackend.model.Funds;
+import tup.CoinControlSinUserBackend.model.PredefinedFund;
+import tup.CoinControlSinUserBackend.model.User;
 
 public interface FundsRepository extends JpaRepository<Funds, Long>{
     List<Funds> findByUserId(Long userId);
+    Funds findByUserAndPredefinedFund(User user, PredefinedFund predefinedFund);
 }
